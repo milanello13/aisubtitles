bb611d38fbcf25a4219ad8045b7cab4976fe82f4
milanello13 <30898831+milanello13@users.noreply.github.com>
2025-12-31 23:18:26 +0300

    Add files via upload

---
+++ installers.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+# =================================================================================================
+# Command: wget https://raw.githubusercontent.com/milanello13/aisubtitles/main/installer.sh -O - | /bin/sh #
+# =================================================================================================
+#########################################################
+version=1.8
+#########################################################
+PACKAGE_DIR='aisubtitles/main'
+MY_FILE="aisubtitles.tar.gz"
+#########################################################
+
+MY_MAIN_URL="https://raw.githubusercontent.com/milanello13/"
+MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_FILE
+MY_TMP_FILE="/tmp/"$MY_FILE
+
+rm -f $MY_TMP_FILE > /dev/null 2>&1
+
+MY_SEP='============================================================='
+echo $MY_SEP
+echo 'Downloading '$MY_FILE' ...'
+echo $MY_SEP
+echo ''
+wget -T 2 $MY_URL -P "/tmp/"
+
+if [ -f $MY_TMP_FILE ]; then
+
+	echo ''
+	echo $MY_SEP
+	echo 'Extracting ...'
+	echo $MY_SEP
+	echo ''
+	tar -xf $MY_TMP_FILE -C /
+	MY_RESULT=$?
+
+	rm -f $MY_TMP_FILE > /dev/null 2>&1
+
+	echo ''
+	echo ''
+	if [ $MY_RESULT -eq 0 ]; then
+        echo "#########################################################"
+        echo "#    Aisubtitles $version INSTALLED SUCCESSFULLY        #"
+        echo "#       Cereated By ammarbary  - support on             #"
+        echo "#  https://www.tunisia-sat.com/forums/threads/4494550/  #"
+        echo "#########################################################"
+        echo "#           your Device will RESTART Now                #"
+        echo "#########################################################"		
+		if which systemctl > /dev/null 2>&1; then
+			sleep 2; systemctl restart enigma2
+		else
+			init 4; sleep 4; init 3;
+		fi
+	else
+		echo "   >>>>   INSTALLATION FAILED !   <<<<"
+	fi;
+	echo ''
+	echo '**************************************************'
+	echo '**                   FINISHED                   **'
+	echo '**************************************************'
+	echo ''
+	exit 0
+else
+	echo ''
+	echo "Download failed !"
+	exit 1
+fi
+# ------------------------------------------------------------------------------------------------------------
